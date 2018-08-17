9ce691a456d157a07dc5f16c1ccb5894268fe1f0
VKrau <atx.korchagin@gmail.com>
2018-08-17 17:51:00 +0300

    modified

--- SUMO/connector.py
+++ SUMO/connector.py
@@ -49,40 +49,54 @@ def checking_network(data, net_file):
     else:
         print("Ok!")
         
-def adding_agents(dictOfAgents):
+def adding_agents(dictOfAgents, step):
     for id_, route in dictOfAgents.iteritems():
-        traci.route.add(str(id_), route)
-        traci.vehicle.add(str(id_), str(id_), typeID="pass")
+        traci.route.add(str(id_+str(step)), route)
+        try:
+            traci.vehicle.add(str(id_), str(id_+str(step)), typeID="pass")
+        except:
+            print("Agent %s has not yet been destroyed" % (id_))    
 
 def run_simulation(network_file, simdata):
     print("--------------LOADING DATA--------------")
     with open(simdata, "rb") as f:
         print("Loading %s" % simdata)
         data = pickle.load(f)
+    print("%s items uploaded" % len(data))
     step = min(data)
     options = get_options()
     max_time = max(data)
+    print("Will be executed %s steps" % (max_time-step))
     if options.nogui:
         sumoBinary = checkBinary("sumo")
     else:
         sumoBinary = checkBinary("sumo-gui")
     sumoCmd = [sumoBinary, "--net-file", "%s" % network_file, 
                "--route-files", "routes.flo.xml",
-               "--step-length",str(1.0*milliseconds_in_step/1000)]
-               #"--full-output","full_xmllog.xml", "--netstate-dump","output_dump.xml",
+               "--step-length",str(1.0*milliseconds_in_step/1000),
+               "--additional-files", "additional.xml"]
+               #"--full-output","full_xmllog.xml", "--netstate-dump","output_dump.xml"]
                #"--device.rerouting.adaptation-steps", "1"]
     print("Ok")
-    print("--------------СHECKING THE NETWORK'S COMPLIANCE WITH ROUTES--------------")
+    print("--------------CHECKING THE NETWORK'S COMPLIANCE WITH ROUTES--------------")
     checking_network(data, network_file)
     print("--------------SIMULATION START--------------")
     traci.start(sumoCmd)
     while step < max_time:
         if step in data:
-            adding_agents(data[step])
+            adding_agents(data[step], step)
         traci.simulationStep()
         step += 1
-    traci.close;
+    traci.close();
     
-run_simulation("mySUMOnetwork1.net.xml", "morning.simdata")
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        run_simulation(sys.argv[1], sys.argv[2])
+    else:
+        try:
+            run_simulation("mySUMOnetwork.net.xml", "morning.simdata")
+        except:
+            print("You need to pass the name of the file with an argument!")
+
 
     
