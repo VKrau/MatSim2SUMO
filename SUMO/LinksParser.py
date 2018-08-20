95f4ff8f952ec99d1d216020819d9a373923a074
VKrau <atx.korchagin@gmail.com>
2018-08-20 17:36:20 +0300

    optimization of the parser

--- SUMO/LinksParser.py
+++ SUMO/LinksParser.py
@@ -3,20 +3,16 @@ import csv
 import sys
 
 def csv_writer(data):
-    with open("Links.csv", "w", newline="") as w_obj:
+    with open("..\Links.csv", "wb") as w_obj:
         writer = csv.writer(w_obj)
         for i in data:
             writer.writerow([i])
 
 def all_links_of_network(file):
     SetSum = set()
-    f = open(file, 'r')
+    f = open(file, 'rb')
     for line in f:
-        Sum = re.findall(r'edge id="-(\w+)"', line)
-        if Sum:
-            Sum[0] = "-"+Sum[0]
-        else:
-            Sum = re.findall(r'edge id="(\w+)"', line)
+        Sum = re.findall(r'\<edge id="([^":]+)', line)
         if Sum:
             SetSum.add(Sum[0])
     f.close()
@@ -25,6 +21,7 @@ def all_links_of_network(file):
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         Links = all_links_of_network(sys.argv[1])
+        print(Links)
         csv_writer(Links)
     else:
         print("You need to pass the name of the file with an argument!")
